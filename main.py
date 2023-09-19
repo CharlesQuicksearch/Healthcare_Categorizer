54bf2997e1b06785bc00ee532ebeded3273b23b8
CharlesQuicksearch <charles.sandholm@quicksearch.se>
2023-09-19 08:27:16 +0200

    added golf_vocabulary

--- main.py
+++ main.py
@@ -2,28 +2,36 @@ from fastapi import FastAPI, HTTPException
 import re
 
 from request_and_response import Response, Request
-from vocabulary import custom_vocabulary
+from vocabulary import healthcare_vocabulary, golf_vocabulary
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/home")
 def home():
     return "Categorization. Send a json with a string to categorize. Ex: '{'input': 'Läkare är trevliga'}"
 
-@app.post("/categorize/", response_model=Response)
+@app.post("/categorize/healthcare", response_model=Response)
 async def categorize(request_data: Request):
     try:
-        output = categorize(request_data.input)
+        output = categorize(request_data.input, healthcare_vocabulary)
         return Response(output = output)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-def categorize(text):
+@app.post("/categorize/golf", response_model=Response)
+async def categorize(request_data: Request):
+    try:
+        output = categorize(request_data.input, golf_vocabulary)
+        return Response(output = output)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+def categorize(text, vocabulary):
     tokens = re.findall(r'\b\w+\b', text.lower())
     mapped_tokens = []
 
     for token in tokens:
-        for common_word, synonyms in custom_vocabulary.items():
+        for common_word, synonyms in vocabulary.items():
             if common_word not in mapped_tokens and token in synonyms:
                 mapped_tokens.append(common_word)
                 break
--- vocabulary.py
+++ vocabulary.py
@@ -1,4 +1,4 @@
-custom_vocabulary = {
+healthcare_vocabulary = {
     "Läkare": ["doctor", "doktron", "doktorn", "doktor", "läkare"],
     "Sjuksköterska": ["skj", "sjuksyster", 'diskriktsköterskorna', 'diskriktsköterska'],
     "Läkemedel": ["medicin", 'mediciner', 'medicinering', 'medicinerna', 'medicinlista',
@@ -6,4 +6,25 @@ custom_vocabulary = {
     "Hemtjänst": ['hemtjänst', 'hemtjänsten'],
     "Hemsjukvård": ['hemsjuksköterskan', 'hemsjukvårdens'],
     "Vårdplan": ["vårdplan", "vårdplanering", "behandlingsstrategi"]
+}
+
+golf_vocabulary = {
+    "Golfbana": ["golfbana", "bana", "banan", "banor", "golfbanan", "golfbanor", "green", "greenen", "greener", "greenerna"
+                 "fairway", "ruff", "ruffen", "tee", "range", "rangen", "träningsområdet",
+                 "träningsområde", "träningsyta", "träningsytan", "avståndsmarkeringar", "avståndsmarkering", "hål", "hålen",
+                "underhåll", "skötsel"],
+
+    "Restaurang": ["Restaurang","resturang", "resturangen", "Restaurangen", "fiket",
+                   "matsalen", "lunch", "lunchen", "luncha", "middag", "mat", "maten", "fika", "fik", "mellanmål", "kiosk",
+                   "kiosken", "kök", "köket", "kökets", "köks", "smaka", "smakade", "middag", "middagen",
+                   "måltiden", "måltid", "kock", "kocken", "kökspersonal", "kökspersonalen", "tillagning",
+                   "tillagad", "tillaga"],
+
+    "Golfklubben": ["Golfklubben", "klubb", "klubben", "golfklubb", "klubbhus", "klubbhuset", "föreningen",
+                    "förening", "golfföreningen", "golfförening", "organisation", "organisationen", "kostnad",
+                    "pris", "priset", "dyr", "dyrt", "dyra"],
+
+    "Personal": ["personal", "personalen", "reception", "receptionen", "receptionisten", "vaktmästare",
+                 "vaktmästaren", "klubbägaren", "klubbägare", "ägaren", "ägare", "skötsel", "bemötande",
+                 "bemött", "bemöta", "bemötta", "anställd", "anställda", "igenkännd"]
 }
